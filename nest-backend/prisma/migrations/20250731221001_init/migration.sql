5f22e9a345b007cefcdfffc3a92e86b7aa3dcbbc
andosoma <soma.ando6669@gmail.com>
2025-08-01 07:18:30 +0900

    prisma導入

---
+++ nest-backend/prisma/migrations/20250731221001_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "public"."samples" (
+    "id" SERIAL NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "samples_pkey" PRIMARY KEY ("id")
+);
